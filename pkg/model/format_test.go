b9071d71e65890cda34b80b8c2592776c2670f7b
Ce Gao <gaoce@caicloud.io>
2020-08-06 13:03:05 +0800

    Update
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/model/format_test.go
+++ pkg/model/format_test.go
@@ -1,63 +1,61 @@
-package model_test
+package model
 
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/kleveross/ormb/pkg/model"
 )
 
 var _ = Describe("Format", func() {
 	It("Should validate SavedModel format successfully", func() {
-		savedmodelFormat := model.FormatSavedModel
+		savedmodelFormat := FormatSavedModel
 		err := savedmodelFormat.ValidateDirectory("../../examples/SavedModel-fashion")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate ONNX format successfully", func() {
-		savedmodelFormat := model.FormatONNX
+		savedmodelFormat := FormatONNX
 		err := savedmodelFormat.ValidateDirectory("../../examples/ONNX-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate H5 format successfully", func() {
-		savedmodelFormat := model.FormatH5
+		savedmodelFormat := FormatH5
 		err := savedmodelFormat.ValidateDirectory("../../examples/H5-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate PMML format successfully", func() {
-		savedmodelFormat := model.FormatPMML
+		savedmodelFormat := FormatPMML
 		err := savedmodelFormat.ValidateDirectory("../../examples/PMML-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate CaffeModel format successfully", func() {
-		savedmodelFormat := model.FormatCaffeModel
+		savedmodelFormat := FormatCaffeModel
 		err := savedmodelFormat.ValidateDirectory("../../examples/Caffe-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate NetDef format successfully", func() {
-		savedmodelFormat := model.FormatNetDef
+		savedmodelFormat := FormatNetDef
 		err := savedmodelFormat.ValidateDirectory("../../examples/Caffe2-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate MXNETParams format successfully", func() {
-		savedmodelFormat := model.FormatMXNETParams
+		savedmodelFormat := FormatMXNETParams
 		err := savedmodelFormat.ValidateDirectory("../../examples/MXNETParams-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate TorchScript format successfully", func() {
-		savedmodelFormat := model.FormatTorchScript
+		savedmodelFormat := FormatTorchScript
 		err := savedmodelFormat.ValidateDirectory("../../examples/TorchScript-model")
 		Expect(err).To(BeNil())
 	})
 
 	It("Should validate GraphDef format successfully", func() {
-		savedmodelFormat := model.FormatGraphDef
+		savedmodelFormat := FormatGraphDef
 		err := savedmodelFormat.ValidateDirectory("../../examples/GraphDef-model")
 		Expect(err).To(BeNil())
 	})
--- pkg/model/model_suite_test.go
+++ pkg/model/model_suite_test.go
@@ -1,4 +1,4 @@
-package model_test
+package model
 
 import (
 	"testing"
